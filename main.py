0a94ba704f3210fd19a442c8b054e2a843b9ab14
w7n <w7o@users.noreply.github.com>
2026-04-12 22:01:19 -0400

    Code to simulate NFA

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import sys
 
 DEBUG = True
 
+def __epsilon(n, states, transitions):
+    set_states = set(states)
+
+    ret = set([])
+    visited = [False] * n
+    while set_states:
+        curr = set_states.pop()
+        visited[curr - 1] = True
+        ret.add(curr)
+        
+        for t in transitions:
+            if (int(t[0]) == curr and t[1] == 'E') and not visited[int(t[2]) - 1]:
+                set_states.add(int(t[2]))
+    return ret
+
+
+
 def main():
     file = None
     input_string = None
@@ -46,6 +63,38 @@ def main():
             f'symbols: {alphabet}\n'
             f'accept states: {accept}\n'
             f'transitions: {transitions}\n======')
+        
+    if(not input_string):
+        input_string = input("Enter input string: ")
+
+    if DEBUG:
+        print(f'Using input \"{input_string}\"...')
+    
+    states = set([start])                  # start simulation at the start state
+    states = (__epsilon(n, states, transitions))
+    i = 0                           # counter for grabbing correct character
+    while(i < len(input_string)):
+        char = input_string[i]      # char: current character being checked
+        next_states = set()
+        # Check all possible routes for transitioning to this character
+        for curr in states:         # curr: curr state being checked
+            for t in transitions:
+                if(int(t[0]) == curr) and t[1] == char:
+                    next_states.add(int(t[2]))
+        next_states = (__epsilon(n, next_states, transitions))
+        states = next_states
+        if(len(states) == 0):
+            print("False")
+            return 0
+        i += 1
+    for accept_state in accept:
+        if accept_state in states:
+            print("True")
+            return 1
+    print("False")
+    return 0
+
+
     
 if __name__=="__main__":
     main()
